7320bf00d608b8569ee05639046a2980bd61a4dd
ufakrendel <64844591+ufakrendel@users.noreply.github.com>
2020-05-09 19:32:07 +0500

    Update hello.py

--- wsgi/hello.py
+++ wsgi/hello.py
@@ -2,7 +2,7 @@ def worker(environ):
         val = str(environ['QUERY_STRING'])
         res =  val.split('&')
         result = '\n'.join(res)
-        return result
+        return bytes(result,'utf-8')
 
 def application(environ, start_response):
     status = '200 OK'
