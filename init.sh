0aeba46bdd443f9aad1ad28bd4a45f2d48c4f930
ufakrendel <64844591+ufakrendel@users.noreply.github.com>
2020-05-09 12:07:21 +0500

    Add files via upload

--- init.sh
+++ init.sh
@@ -1,15 +1,9 @@
-cd /home
 mkdir -p /home/box/web/public/img
-mkdir -p /home/box/web/public/css
-mkdir -p /home/box/web/public/js
-mkdir -p /home/box/web/uploads
-mkdir -p /home/box/web/etc
+cd /home/box/web/public/
+mkdir css
+mkdir js
+cd /home/box/web/
+mkdir uploads
+mkdir etc
 
-find /home/box/ -type d -exec chmod 755 {}
-find /home/box/ -type f -exec chmod 644 {}
-
-cp ./mNgingx.conf /home/box/web/etc/nginx.conf
-
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
-sudo /etc/init.d/nginx restart
 
