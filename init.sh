5c1050d910f76eb48dad63c4cdce1a73c6827ab8
ufakrendel <64844591+ufakrendel@users.noreply.github.com>
2020-05-11 12:26:46 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,8 +8,10 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
 sudo rm /etc/nginx/sites-enabled/default
 
 cp ./wsgi/hello.py /home/box/web/hello.py
-cp ./wsgi/wsgi.config /home/box/web/etc/hello.py
 rm /etc/gunicorn.d/*
+cp ./wsgi/wsgi.config /home/box/web/etc/hello.py
+gunicorn -c /home/box/web/etc/hello.py /home/box/web/hello:application --daemon
+
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
 sudo nginx
