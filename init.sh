107ec92bffb7b2c11ac9d53c7753e4588300abb7
ufakrendel <64844591+ufakrendel@users.noreply.github.com>
2020-05-11 11:54:15 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,9 +4,14 @@ mkdir -p /home/box/web/public/js
 mkdir -p /home/box/web/uploads
 mkdir -p /home/box/web/etc
 cp ./nginx.conf /home/box/web/etc/nginx.conf
-cp ./wsgi/hello.py /home/box/web/hello.py
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
 sudo rm /etc/nginx/sites-enabled/default
+
+cp ./wsgi/hello.py /home/box/web/hello.py
+cp ./wsgi/wsgi.config /home/box/web/etc/hello.py
+rm /etc/gunicorn.d/*
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+
 sudo nginx
 
 
