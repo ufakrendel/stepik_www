4399a60ab120efeddf86c0a467b96a81e23a20e2
ufakrendel <64844591+ufakrendel@users.noreply.github.com>
2020-05-18 18:52:06 +0500

    легкий инит. Потом грохнуть

---
+++ linit.sh
@@ -0,0 +1,17 @@
+  
+mkdir -p /home/box/web/public/img
+mkdir -p /home/box/web/public/css
+mkdir -p /home/box/web/public/js
+mkdir -p /home/box/web/uploads
+mkdir -p /home/box/web/etc
+cp ./nginx.conf /home/box/web/etc/nginx.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
+sudo rm /etc/nginx/sites-enabled/default
+ 
+
+cd /home/box/web/
+django-admin startproject ask
+cd ask
+./manage.py startapp qa
+
+ 
